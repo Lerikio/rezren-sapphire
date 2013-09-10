ccb08826bf8965cc119f090fde0eb3da8313b780
Expyron <expyron@gmail.com>
2013-09-10 03:48:49 +0200

    Changement du secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sapphire::Application.config.secret_token = 'ccd48c65183bba1df490d7ab5e7d2aea1266539002e4507c57bf29bc118a486fa5228776f32dd09285a58031236a217e2d174fc9840608aad7405c7699740fee'
+Sapphire::Application.config.secret_token = 'ccd48c6518bbba1df490d7ab5e7d2aea1266539002e4507c57bf29bc118a486fa5228776f32dd09285a58031236a217e2d174fc9840608aad7405c7699740fee'
