ed860dc1b275d63be54c3715883196a63ef0583c
Expyron <tom.gonel@gmail.com>
2013-10-11 22:05:34 +0200

    add to array

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,8 @@
 class ActivitiesController < ApplicationController
 
   def index
-  	PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
+  	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
+  	@activities << @payments
   end
 end
