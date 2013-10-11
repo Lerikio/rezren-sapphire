e24c7f0ea4375ef4fe50967ce044657617a0813d
Expyron <tom.gonel@gmail.com>
2013-10-11 22:02:39 +0200

    Eager loading

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@
 class ActivitiesController < ApplicationController
 
   def index
+  	PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
   end
 end
