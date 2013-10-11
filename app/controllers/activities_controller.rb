b3095b90450ce6ff9f3d5617887a0aa0a3213e62
Expyron <tom.gonel@gmail.com>
2013-10-11 22:33:38 +0200

    Correction

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,5 @@ class ActivitiesController < ApplicationController
   def index
   	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
-  	@activities = @activities + @payments
   end
 end
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,3 +1,7 @@
 # -*- encoding : utf-8 -*-
 module ActivitiesHelper
+	#Pour l'eager loading
+	def payment_from_trackable(trackable)
+		@payments.detect{|p| p.id == trackable.id}
+	end
 end
