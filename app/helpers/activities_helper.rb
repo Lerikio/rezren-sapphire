5c4195303265266619e08cac107f241ceed554b3
Expyron <tom.gonel@gmail.com>
2013-10-11 22:43:58 +0200

    Correction

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -3,6 +3,6 @@ module ActivitiesHelper
 	#Pour l'eager loading
 	def payment_from_trackable(trackable)
 		payment = @payments.detect{|p| p.trackable.id == trackable.id}
-		payment.id
+		payment.trackable
 	end
 end
