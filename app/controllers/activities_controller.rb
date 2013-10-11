b24e44fd3a7f6641ac341910f1dc888a26ad4f73
Expyron <tom.gonel@gmail.com>
2013-10-11 22:23:46 +0200

    Revert "Correction"
    This reverts commit 0231e70ae6e6af86c89363393efd69f88c1bd2e2.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,11 +4,6 @@ class ActivitiesController < ApplicationController
   def index
   	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
-  	@activities.map! { |a| 
-  		if a.class.name == "Payment" then
-  			return @payments.detect{|p| p.id == a.trackable.id) }
-		end
-		a
-  	}
+  	@activities = @activities + @payments
   end
 end
