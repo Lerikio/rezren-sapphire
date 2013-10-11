0231e70ae6e6af86c89363393efd69f88c1bd2e2
Expyron <tom.gonel@gmail.com>
2013-10-11 22:20:13 +0200

    Correction

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,11 @@ class ActivitiesController < ApplicationController
   def index
   	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
-  	@activities = @activities + @payments
+  	@activities.map! { |a| 
+  		if a.class.name == "Payment" then
+  			return @payments.detect{|p| p.id == a.trackable.id) }
+		end
+		a
+  	}
   end
 end
