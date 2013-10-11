b15bf1e3d4cc8b2c4ea8f4cb5b397313f5077ad1
Expyron <tom.gonel@gmail.com>
2013-10-11 22:40:06 +0200

    Sorting

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@
 class ActivitiesController < ApplicationController
 
   def index
-  	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
+  	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).order("created_at desc").find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
   end
 end
