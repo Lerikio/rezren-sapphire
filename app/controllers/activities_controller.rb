81bbb7bd8a11a5b4b51e902833151405d8415982
Expyron <tom.gonel@gmail.com>
2013-10-11 22:07:18 +0200

    Correction

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,6 @@ class ActivitiesController < ApplicationController
   def index
   	@payments = PublicActivity::Activity.includes(:owner, [:trackable => :adherent]).limit(200).find_all_by_trackable_type("Payment")
   	@activities = PublicActivity::Activity.includes(:owner, :trackable).limit(200).order("created_at desc")
-  	@activities << @payments
+  	@activities = @activities + @payments
   end
 end
